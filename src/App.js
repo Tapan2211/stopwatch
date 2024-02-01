e9885c60fe90efa46f4de716c010518a01182160
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2024-02-01 13:23:31 +0530

    first commit stopwatch app

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [elaspedtime, setElaspedTime] = useState(0);
+
+  const formateTime = (secs) => {
+    const mins = Math.floor(secs / 60);
+    const remainingSecs = secs % 60;
+    return `${mins} : ${remainingSecs < 10 ? "0" : " "} ${remainingSecs}`
+  }
+
+  const toggleTime = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  }
+
+  const reset = () => {
+    setIsRunning(false);
+    setElaspedTime(0);
+  }
+
+  useEffect(() => {
+    const ac = new AbortController();
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElaspedTime((prevElaspedTime) => prevElaspedTime + 1)
+      }, 1000)
+    } else {
+      clearInterval(intervalId);
+    }
+    return()=> {clearInterval(intervalId);
+    ac.abort();
+  }
+  }, [isRunning])
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <p>Time: {formateTime(elaspedtime)}</p>
+      <button onClick={toggleTime}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
