a1ea6327beb404e2943c39abca23fa798f7ab401
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2024-02-01 17:45:25 +0530

    first commit on stopwatch app

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ function App() {
   }
   }, [isRunning])
 
+  //git remote set-url origin https://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@github.com/Tapan2211/stopwatch
+
   return (
     <div className="App">
       <h1>Stopwatch</h1>
